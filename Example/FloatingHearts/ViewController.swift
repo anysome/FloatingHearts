9c63112000d9753eddc53fc41b81f8c110ddb036
Layman <anysome@gmail.com>
2017-08-18 12:51:55 +0800

    pod and swift 3

--- Example/FloatingHearts/ViewController.swift
+++ Example/FloatingHearts/ViewController.swift
@@ -1,9 +1,9 @@
 //
 //  ViewController.swift
-//  FloatingHeartsExample
+//  FloatingHearts
 //
-//  Created by Said Marouf on 4/20/16.
-//  Copyright © 2016 Said Marouf. All rights reserved.
+//  Created by anysome on 08/18/2017.
+//  Copyright (c) 2017 anysome. All rights reserved.
 //
 
 import UIKit
@@ -13,42 +13,42 @@ class ViewController: UIViewController {
 
     private struct HeartAttributes {
         static let heartSize: CGFloat = 36
-        static let burstDelay: NSTimeInterval = 0.1
+        static let burstDelay = 0.1
     }
-
-    var burstTimer: NSTimer?
-
+    
+    var burstTimer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = UIColor(hex: 0xf2f4f6)
+        view.backgroundColor = UIColor.black
 
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(showTheLove))
         view.addGestureRecognizer(tapGesture)
-
+        
         let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(didLongPress))
         longPressGesture.minimumPressDuration = 0.2
         view.addGestureRecognizer(longPressGesture)
     }
-
+    
     func didLongPress(longPressGesture: UILongPressGestureRecognizer) {
         switch longPressGesture.state {
-        case .Began:
-            burstTimer = NSTimer.scheduledTimerWithTimeInterval(HeartAttributes.burstDelay, target: self, selector: #selector(showTheLove), userInfo: nil, repeats: true)
-        case .Ended, .Cancelled:
+        case .began:
+            burstTimer = Timer.scheduledTimer(timeInterval: HeartAttributes.burstDelay, target: self, selector: #selector(showTheLove), userInfo: nil, repeats: true)
+        case .ended, .cancelled:
             burstTimer?.invalidate()
         default:
             break
         }
     }
-
+    
     func showTheLove(gesture: UITapGestureRecognizer?) {
-        let heart = HeartView(frame: CGRectMake(0, 0, HeartAttributes.heartSize, HeartAttributes.heartSize))
+        let heart = HeartView(frame: CGRect(x:0, y:0, width:HeartAttributes.heartSize, height:HeartAttributes.heartSize))
         view.addSubview(heart)
-        let fountainX = HeartAttributes.heartSize / 2.0 + 20
+        let fountainX = view.frame.width - 65
         let fountainY = view.bounds.height - HeartAttributes.heartSize / 2.0 - 10
         heart.center = CGPoint(x: fountainX, y: fountainY)
         heart.animateInView(view)
     }
-    
+
 }
+
